4dcf0e3d7f1a938912330da277b96dc2af6ddcf0
Daniil <kbootovsk@gmail.com>
2025-08-13 14:19:02 +0300

    Add home and dashboard pages

--- adaptive_automation/urls.py
+++ adaptive_automation/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import home, dashboard
 
 urlpatterns = [
+    path('', home, name='home'),
+    path('dashboard/', dashboard, name='dashboard'),
     path('admin/', admin.site.urls),
     path('api/metrics/', include('metrics.urls')),
     path('api/integrations/', include('integrations.urls')),
